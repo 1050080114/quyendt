518e4d2ca3b566f1d97bcbd3d38572b427d4841a
1050080114 <125208717+1050080114@users.noreply.github.com>
2023-03-05 22:23:17 +0700

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab04;
+
+/**
+ *
+ * @author Laptop
+ */
+public class  {
+
+    public class Circle {
+    private double radius;
+    private String color="White";
+    public Circle(){
+        this.radius=1.0;
+        this.color="White";
+    }
+    public double getradius(){
+        return this.radius;
+    }
+    public void setradius(double radius){
+        this.radius=radius;
+    }
+    public String getcolor(){
+        return this.color;
+    }
+    public String setcolor(String color){
+        return this.color;
+    }
+    public String tostring(){
+        return "Cricle[radius="+radius+",color="+color+"]";
+    }
+    public double getArae(){
+        return radius=radius*radius*Math.PI;
+    }
+}
+        // TODO code application logic here
+    }
+    
+
